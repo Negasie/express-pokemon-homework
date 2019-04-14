803178d93f3b2dd9921b7307d9432e5cc357f67b
Nick Johnson <Blackjacknick@gmail.com>
2019-04-14 10:26:22 -0600

    Commit 1 - My server is set up and running

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const app = express();
+
+
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+app.listen(port, function() {
+  console.log("App is listening");
+});
+
+app.get("/", function (req, res) {
+  res.send("I am listening");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
